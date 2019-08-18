32574767c818bfd538cef733a3f56ec64a8ceaf4
Wentao Zheng <x1019575585@163.com>
2019-08-18 14:04:38 +0800

    Update Github_Stargazers_Crawler.py

--- Github_Stargazers_Crawler.py
+++ Github_Stargazers_Crawler.py
@@ -21,33 +21,48 @@ class github(object):
         self.star_num = int(temp1.text.split()[0].replace(",",""))
         self.name_list = None
         r.keep_alive = False
+
     def get_list(self):
-        j = 1
         url2 = self.url
         self.name_list = []
+        self.info_list = []
         temp = self.star_num//30+1
-        for i in range(temp):
-            if len(self.name_list) > self.star_num-1:
+        for k in range(temp):
+            if len(self.info_list) > self.star_num-1:
                 break
             attempts = 0
             success = False
             while attempts < 10 and not success:
+                if len(self.info_list) > self.star_num-1:
+                    break
                 try:
                     r = requests.get(url2)
                     demo = r.text
                     soup = BeautifulSoup(demo, "html.parser")
-                    temp1 = soup.find_all('h3', class_="follow-list-name")
+                    temp1 = soup.find_all('p', class_="follow-list-info")
+                    temp2 = soup.find_all('h3', class_="follow-list-name")
                     i = 0
                     while i < len(temp1):
-                        temp1_2=temp1[i].find('a')
-                        i = i+1
-                        self.name_list.append(temp1_2.text)
-                        print(self.name,':      star_num:  ',self.star_num,'      name:',temp1_2.text,' round: ', str(j),'  num: ',str(i),"   Done")
+                        temp2_1=temp2[i].find('a')
+                        self.name_list.append(temp2_1.text)
+                        print(self.name,':      star_num:  ',self.star_num,'      name:',temp2_1.text,' round: ', str(k),'  num: ',str(i),"   Done")
+                        temp1_1 = temp1[i].find('span')
+                        if temp1_1 != None:
+                            temp1_2 = temp1[i].find('span')
+                            self.info_list.append(temp1_2.text)
+                            print(self.name,':      star_num:  ',self.star_num,'      name:',temp1_2.text,' round: ', str(k),'  num: ',str(i),"   Done")
+                        elif temp1[i].text != None:
+                            text = temp1[i].text
+                            self.info_list.append(text)
+                            print(self.name,':      star_num:  ',self.star_num,'      name:',text,' round: ', str(k),'  num: ',str(i),"   Done")
+                        else:
+                            self.info_list.append(np.nan)
+                            print(self.name,':      star_num:  ',self.star_num,'      name:  NAN  round: ', str(k),'  num: ',str(i),"   Done")
+                        i = i + 1
                     temp2=soup.find('a', rel="nofollow", class_="btn btn-outline BtnGroup-item",text = 'Next')
                     url2 = temp2['href']
                     r.keep_alive = False
                     success = True
-                    j = j + 1
                 except BaseException as e:
                     print(e)
                     attempts += 1
@@ -59,14 +74,17 @@ class github(object):
                         time.sleep(120)
                     if attempts >= 10:
                         break
-
+        self.info_list = pd.DataFrame(self.info_list,index = self.name_list, names = "info" )
         pd.Series(self.name_list).to_csv('github_'+self.name+'.txt')
+        self.info_list.to_csv('github_info_'+self.name+'.csv')
 
     def get_table(self):
         if self.name_list == None:
             name_list = list(pd.read_csv('github_'+self.name+'.txt',header = None).iloc[:,1])
         else:
             name_list = self.name_list
+        if self.info_list == None:
+            self.info_list = pd.read_csv('github_info_'+self.name+'.csv',index_col = 0, names = 'info')#header=None
         self.result = pd.DataFrame(np.full([len(name_list),6], np.nan),columns = ["Repositories","Projects",\
                               "Stars", "Followers","Following","Contributions in the last year"])
         self.result.index = pd.Series(name_list)
@@ -109,6 +127,8 @@ class github(object):
                     if attempts >= 10:
                         break
         self.result.to_csv('github_'+self.name+'.csv')
+        self.result2 = pd.merge(self.result, self.info_list, left_index=True, right_index=True)
+        self.result.to_csv('github_tot_'+self.name+'.csv')
 
     def run(self):
         self.get_list()
@@ -120,9 +140,9 @@ if __name__ == '__main__':
 #    star=github('https://github.com/pingcap/tidb/stargazers')  #3
 #    star=github('https://github.com/influxdata/telegraf/stargazers')  #4
 #    star=github('https://github.com/Kong/kong/stargazers')  #5
-#    star=github('https://github.com/hashicorp/terraform/stargazers')   #6
+    star=github('https://github.com/hashicorp/terraform/stargazers')   #6
 #    star=github('https://github.com/elastic/elasticsearch/stargazers')    #7
-    star=github('https://github.com/mongodb/mongo/stargazers')   #8
+#    star=github('https://github.com/mongodb/mongo/stargazers')   #8
     print("star num: ",str(star.star_num))
 #    star.get_list()
 #    star.get_table()
